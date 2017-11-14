ad7d08303a8070c472b344c5121eb5322594afb9
yuban12315 <fengyulin@imudges.com>
2017-11-14 16:09:32 +0800

    将项目从express更改为webpack，并成功打包了css文件

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-layout>
+    <h1 class="h1">Welcome home</h1>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+  import '../main.css'
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
